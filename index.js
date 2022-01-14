4dd4d3268a316754bcb0520150030e0c8f0fc694
Alex Manzi <alex.manzi22@gmail.com>
2022-01-14 14:46:06 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+let add = (a,b) => {
+    return (a+b)
+}
+let subtract = (a,b) => {
+    return (a-b)
+}
+let multiply = (a,b) => {
+    return (a*b)
+}
+let divide = (a,b) => {
+    return (a/b)
+}
+
+
+let increment = (n) => {
+    return (++n)
+}
+
+let decrement = (n) => {
+    return (--n)
+}
+
+let makeInt = (n) => {
+    return parseInt(n, 10)
+}
+let preserveDecimal = (n) => {
+    return parseFloat(n)
+}
